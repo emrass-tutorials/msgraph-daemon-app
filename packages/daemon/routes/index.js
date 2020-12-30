44c06ed8acbf8b2f990a5ed504f13f2d0082a746
Enrico Mrass <emrass@users.noreply.github.com>
2020-12-29 22:30:12 -0800

    Add styling and basic web app content

--- packages/daemon/routes/index.js
+++ packages/daemon/routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  let params = {
+    active: { home: true },
+  };
+
+  res.render("index", params);
 });
 
 module.exports = router;
